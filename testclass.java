2f8e90b5907ad67ee2a55f0eb075870dcf1987b9
duhwan1001 <kdhz2012@gmail.com>
2022-10-29 13:44:54 +0900

    push from main branch

--- testclass.java
+++ testclass.java
@@ -2,7 +2,7 @@ public class testclass {
 
     public static void main(String[] args) {
         // sub에서 작성한거
-        String hi2 = "hi from sub branch";
+        String hi1 = "hi from main branch";
 
 
 
